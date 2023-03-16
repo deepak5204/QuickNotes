a3d35cbb4ac1defbc9eea27bce14fbc26c3d6335
Deepak <deepakkumar750206@gmail.com>
2023-03-16 16:24:20 +0530

    create a post route for checking

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();  //create a new router object
 const port = 8000;
 
+//create question
+app.post('/add', (req, res) => {
+    res.status(200).json({
+        message: 'hello from post route'
+    });
+});
 
 app.listen(port, ()=>{
     console.log(`server is running on port ${port}`); 
